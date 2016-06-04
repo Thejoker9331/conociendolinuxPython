80820d67cf8311693e1906190e805466f87e57af
Enrique Gudiño <Thejoker9331@users.noreply.github.com>
2016-06-04 13:34:36 -0500

    Python 3
    Este es muy pequeño ejemplo de python 3. 
    Pueden encontrar este y mucho mas ejemplos en conociendolinux.esy.es/Linux/cursos.php

---
+++ ejemplo.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python 
+# -*- coding: utf-8 -*-
+#Primer programa en python 3
+nombre = input("¿Como te llamas?") #Almacenamos la respuesta con ayuda de input para capturar lo ingresado y lo almacenamos en la variable "nombre".
+print ("Bienvenido", nombre) #imprimimos a pantalla lo que recibio la variable nombre.
+
+valor_pi=3.1416 #Número real.
+print (valor_pi) #imprimimos a pantalla lo que recibio la variable edad.
